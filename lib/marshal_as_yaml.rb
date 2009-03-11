9db70f97b3a63abadbe8bfa2cc3d9c9ec0572df8
Bradford Folkens <bfolkens@gmail.com>
2009-03-10 20:05:16 -0500

    Initial commit

---
+++ lib/marshal_as_yaml.rb
@@ -0,0 +1,21 @@
+module MarshalAsYaml
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def marshal_as_yaml(*attrs)
+      attrs = [attrs] unless attrs.is_a?(Array)
+      attrs.each do |attr|
+        define_method attr do
+          return nil if read_attribute(attr).nil?
+          YAML.load read_attribute(attr)
+        end
+      
+        define_method "#{attr}=" do |val|
+          write_attribute attr, (val && val.to_yaml)
+        end
+      end
+    end
+  end
+end
